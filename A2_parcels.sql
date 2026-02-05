38ab57e6f01fcb5d986361793274f38c66faa963
raszyroo <nj7353nv@go.minnstate.edu>
2026-02-05 12:53:11 -0600

    Update A2_parcels.sql
    built view of query of sale price and acres

--- A2_parcels.sql
+++ A2_parcels.sql
@@ -1,1 +1,16 @@
-
+CREATE OR REPLACE VIEW student.v_parcels_final AS
+SELECT
+	defaultdb.enterprise.parcels_core.pin,
+	defaultdb.enterprise.parcels_core.acres_poly,
+	defaultdb.enterprise.parcels_core.use1_desc,
+	defaultdb.enterprise.parcel_sales.sale_date,
+	defaultdb.enterprise.parcel_sales.sale_value,
+	defaultdb.enterprise.parcels_core.shape
+FROM
+	defaultdb.enterprise.parcels_core
+LEFT JOIN
+	defaultdb.enterprise.parcel_sales
+ON  defaultdb.enterprise.parcel_sales.pin = defaultdb.enterprise.parcels_core.pin
+WHERE
+	defaultdb.enterprise.parcel_sales.sale_value > 300000 AND
+	defaultdb.enterprise.parcels_core.acres_poly < 1;
